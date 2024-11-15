c927070606437fe836701b70f61be575b7183d3e
Vincenzo <Marchese>
2024-11-15 17:57:33 +0100

    fix: clean ingress and egress

--- cmd/main.go
+++ cmd/main.go
@@ -129,20 +129,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Identity")
 		os.Exit(1)
 	}
-	if err = (&controller.EgressReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Egress")
-		os.Exit(1)
-	}
-	if err = (&controller.IngressReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Ingress")
-		os.Exit(1)
-	}
 	if err = (&controller.AzureProviderReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
